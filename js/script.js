6d2f429037bcc06593970a5f862307f684c7a69c
Jimmy Wu <cwu023@gmail.com>
2018-02-15 13:09:29 -0800

    Shortern the quotes and added js logics

--- js/script.js
+++ js/script.js
@@ -5,47 +5,67 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 //This array of objects holds the data for the quotes
 var quotes = [
 	{
-		quote: "I have self-doubt. I have insecurity. I have fear of failure. I have nights when I show up at the arena and I'm like, 'My back hurts, my feet hurt, my knees hurt. I don't have it. I just want to chill.' We all have self-doubt. You don't deny it, but you also don't capitulate to it. You embrace it.",
+		quote: "We all have self-doubt. You don't deny it, but you also don't capitulate to it. You embrace it.",
 		source: "Kobe Bryant",
+		citation: "",
+		year: ""
 	},
 
 	{
-		quote: "For the past 33 years, I have looked in the mirror every morning and asked myself: 'If today were the last day of my life, would I want to do what I am about to do today?' And whenever the answer has been 'No' for too many days in a row, I know I need to change something.",
+		quote: "Innovation distinguishes between a leader and a follower.",
 		source: "Steve Jobs",
+		citation: "",
+		year: ""
 	},
 
 	{
 		quote: "I can accept failure, everyone fails at something. But I can't accept not trying.",
 		source: "Michael Jordan",
+		citation: "",
+		year: ""
 	},
 
 	{
 		quote: "The function of education is to teach one to think intensively and to think critically. Intelligence plus character - that is the goal of true education.",
 		source: "Martin Luther King, Jr.",
+		citation: "",
+		year: ""
 	},
 
 	{
 		quote: "The most beautiful thing we can experience is the mysterious. It is the source of all true art and science.",
 		source: "Albert Einstein",
+		citation: "",
+		year: ""
 	}
 
 ]
 
-/*
--This function selects a random quote object from the the quotes array
--Returns the random quote object
-*/	
-function getRandomQuote() = {
 
+//This function selects a random quote object from the the quotes array
+//Returns the random quote object	
+function getRandomQuote() {
+	var randomQuotes = Math.floor(Math.random() * quotes.length);
+	return quotes[randomQuotes];
 }
 
-/*
-- This function calls the getRandomQuote function and stores the returned quote object in a variable
-- This function constructs a string containing the different properties of the quote object	
- */
-function printQuote() = {
 
+//This function calls the getRandomQuote function and stores the returned quote object in a variable
+//This function constructs a string containing the different properties of the quote object	
+function printQuote() {
+	var returnedQuotes = getRandomQuote();
+	var myQuotes = '<p class="quote">' + returnedQuotes.quote + '</p>';
+	myQuotes += '<p class="source">' + returnedQuotes.source;
+	//if citation or year is missing, only add </p> to the quotes
+	if (returnedQuotes.citation === "" || returnedQuotes.year === "") {
+		myQuotes += '</p>';
+	} 
+
+	//if citation or year is not missing, add to quotes with the </p>
+	else {
+		myQuotes += '<span class="citation">' + returnedQuotes.citation + '</span>' + '</p>';
+		myQuotes += '<span class="year">' + returnedQuotes.year + '</span>' + '</p>';
+	}
+	document.getElementById('quote-box').innerHTML = myQuotes;
 } 
-
-
 
